e55035a38caf77cc25c064b1f858a481e6d6af50
Benson Zheng <benson515@sbcglobal.net>
2019-06-07 17:07:32 -0700

    card one and card 2 ready

--- script.js
+++ script.js
@@ -6,9 +6,19 @@ $( document ).ready(function() {
     let secondCard = "second-card.html"
     let thirdCard = "third-card.html"
 
+    let project1 = "project-card-1.html"
+    let project2 = "project-card-2.html"
+
+    
     $("#nav").load(nav)
     $("#first-card").load(firstCard)
-    $("#second-card").load(secondCard)
+
+    $("#second-card").load(secondCard, function(){
+        $("#project-card-1").load(project1)
+        // $("#project-card-2").load(projectCard2)
+        // $("#project-card-3").load(projectCard2)
+        
+    })
     $("#third-card").load(thirdCard)
     
 
