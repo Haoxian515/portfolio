8b54928f24cf9d36d6c6e999cf1ffc9620730c88
Benson Zheng <benson515@sbcglobal.net>
2019-10-29 17:09:16 -0700

    toggles

--- script.js
+++ script.js
@@ -13,11 +13,28 @@ $( document ).ready(function() {
     
     $("#nav").load(nav)
     $("#first-card").load(firstCard)
+                $("#preview-right-column").hide()
 
     $("#second-card").load(secondCard, () => {
-        $("#project-card-1").load(flight_visualiser)
-        $("#project-card-2").load(gator_lodge)
-        $("#project-card-3").load(open_rice)
+        $("#project-card-1").load(flight_visualiser, () => {
+            $("#flight").hide()
+            $("#flight-btn").click(() => {
+                $("#flight").toggle()
+            })
+        })
+
+        $("#project-card-2").load(gator_lodge, () => {
+            $("#lodge").hide()
+            $("#lodge-btn").click(() => {
+                $("#lodge").toggle()
+            })
+        })
+        $("#project-card-3").load(open_rice, () => {
+            $("#rice").hide()
+            $("#rice-btn").click(() => {
+                $("#rice").toggle()
+            })
+        })
     })
     $("#third-card").load(thirdCard)
     
