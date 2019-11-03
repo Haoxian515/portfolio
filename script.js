0739aabdf9340e3caeaae0e890596134a5654cb7
Benson Zheng <benson515@sbcglobal.net>
2019-11-02 22:18:04 -0700

    ui

--- script.js
+++ script.js
@@ -34,7 +34,24 @@ function rotateB(){
 }
 
 
+function showDetail(e){
 
+    //IMG
+    $(e.getElementsByClassName("project-img")[0]).animate({opacity:0})
+
+    //CONTENT
+    $(e.getElementsByClassName("project-content-top")[0]).css("visibility","visible")
+    $(e.getElementsByClassName("project-content-bot")[0]).css("visibility","visible")
+
+}
+
+function unshowDetail(e){
+
+    $(e.getElementsByClassName("project-img")[0]).animate({opacity:1})
+    $(e.getElementsByClassName("project-content-top")[0]).css("visibility","hidden")
+    $(e.getElementsByClassName("project-content-bot")[0]).css("visibility","hidden")
+
+}
 
 // **document ready
 $( document ).ready(function() {
@@ -49,6 +66,8 @@ $( document ).ready(function() {
     let open_rice = "./project_cards/open-rice.html"
     let gator_lodge = "./project_cards/gator-lodge.html"
 
+    let flightV2 = "./project_cards/flightv2.html"
+
     $("#nav").load(nav)
 
     
@@ -57,25 +76,33 @@ $( document ).ready(function() {
     })
 
     $("#projects").load(projects, () => {
-        $("#project-card-1").load(flight_visualiser, () => {
-            $("#flight").hide()
-            $("#flight-btn").click(() => {
-                $("#flight").fadeToggle(500)
-            })
+        $("#project-card-1").load(flightV2, () => {
+            // $(".project-card").mouseover((e)=>{
+            //     $(this).find('.project-content').css("visibility","visible");
+            // })
+            // $(".project-card").mouseout((e)=>{
+            //     $(this).find('.project-content').css("visibility","visible");
+            // })
         })
+        // $("#project-card-1").load(flight_visualiser, () => {
+        //     $("#flight").hide()
+        //     $("#flight-btn").click(() => {
+        //         $("#flight").fadeToggle(500)
+        //     })
+        // })
 
-        $("#project-card-2").load(gator_lodge, () => {
-            $("#lodge").hide()
-            $("#lodge-btn").click(() => {
-                $("#lodge").fadeToggle(500)
-            })
-        })
-        $("#project-card-3").load(open_rice, () => {
-            $("#rice").hide()
-            $("#rice-btn").click(() => {
-                $("#rice").fadeToggle(500)
-            })
-        })
+        // $("#project-card-2").load(gator_lodge, () => {
+        //     $("#lodge").hide()
+        //     $("#lodge-btn").click(() => {
+        //         $("#lodge").fadeToggle(500)
+        //     })
+        // })
+        // $("#project-card-3").load(open_rice, () => {
+        //     $("#rice").hide()
+        //     $("#rice-btn").click(() => {
+        //         $("#rice").fadeToggle(500)
+        //     })
+        // })
     })
     // $("#third-card").load(thirdCard)
     
